654e991d77bd2ca8ec8091a97b3d8056f6cfd51a
kadeshiro <gaaadaaa16@gmail.com>
2020-06-17 01:35:58 +0900

    [fix]lazyにする

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,48 @@
-import React from "react";
+import React, { lazy, useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import AppLayout from "./components/AppLayout";
-import Main from "./pages/Main";
-import Start from "./pages/Start";
-import Error from "./pages/Error";
 import { appState } from "./actions/app";
 import { StateType } from "./reducers";
 import { APP_STATE_TYPE } from "./actions/app";
+
+const Start = lazy(() => import("./pages/Start"));
+const Error = lazy(() => import("./pages/Error"));
+const Main = lazy(() => import("./pages/Main"));
+
+const getPage = async (state: APP_STATE_TYPE) => {
+  switch (state) {
+    case appState.WAIT_START: {
+      return <Start />;
+    }
+    case appState.APP_ERROR: {
+      return <Error />;
+    }
+    default: {
+      return <Main state={state} />;
+    }
+  }
+};
 
 const App = () => {
   const {
     app: { appState: state },
   } = useSelector((state: StateType) => state);
-  const page = (state: APP_STATE_TYPE) => {
-    switch (state) {
-      case appState.WAIT_START:
-        return <Start />;
-      case appState.APP_ERROR:
-        return <Error />;
-      default:
-        return <Main state={state} />;
-    }
-  };
-  return <AppLayout>{page(state)}</AppLayout>;
+  const [view, setView] = useState<any>(null);
+
+  useEffect(() => {
+    const loadPages = async () => {
+      const page = await getPage(state);
+      setView(page);
+    };
+    loadPages();
+  }, [state]);
+
+  return (
+    <AppLayout>
+      <React.Suspense fallback="Loading...">{view}</React.Suspense>
+    </AppLayout>
+  );
 };
 
 export default App;
